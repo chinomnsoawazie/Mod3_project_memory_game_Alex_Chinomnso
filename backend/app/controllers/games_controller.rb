eb72e940e1e25c15f439d68203fe44b6ad7dbbfe
awazienommy <coawazie@gmail.com>
2019-11-14 07:47:35 -0500

    add create and path methods in controllers

--- backend/app/controllers/games_controller.rb
+++ backend/app/controllers/games_controller.rb
@@ -8,4 +8,48 @@ class GamesController < ApplicationController
         game = Game.find(params[:id])
         render json: game
     end
+
+    def new
+        @game = Game.new
+    end
+
+    def edit 
+    end
+
+    # POST/games.json
+    def create
+        @game = Game.new(game_params)
+        respond_to do |format|
+            if @game.save
+                format.html {redirect_to, notice: 'Game was succesfully created'}
+                format.json {render :show, status: :created, location: @game}
+            else
+                format.html {render :new}
+                format.json {render json: @game.errors, status: :unprocessable_entity}
+            end
+        end
+    end
+
+    # PATCH/toys/1.json
+    def update
+        respond_to do |format|
+            if @game.update(game_params)
+                format.html {redirect_to @game, notice: "Game successfully updated."}
+                format.json {render :show, status: :ok, location: @game}
+            else
+                format.html {render :edit}
+                format.json {render json: @game.errors, status: :unprocessable_entity}
+            end
+        end
+    end
+
+
+    private
+    def set_game
+        @game = Game.find(params[:id])
+    end
+
+    def game_params
+        params.require(:game).permit(:points, :level, :winstatus)
+    end
 end
--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,11 +1,44 @@
 class UsersController < ApplicationController
+    before_action :set_toy, only: [:show, :edit, :update, :destroy]
     def index
-        users = User.all
-        render json: users
+        @users = User.all
+        render json: @users
+    end
+    
+    def show
+        @user = User.find(params[:id])
+        render json: @user
     end
 
-    def show
-        user = User.find(params[:id])
-        render json: user
+    def new
+        @user = User.new
+    end
+
+    def edit 
+    end
+
+    def create
+        @user = User.new(user_params)
+        respond_to do |format|
+            if @user.save
+                format.html { redirect_to @user, notice: 'User successfully created.'}
+                format.json {render :show, status: :created, location: @user}
+            else
+                format.html {render :new}
+                format.json {render json: @user.erros, status: :unprocessable_entity}
+            end
+        end
+    end
+
+    
+
+
+    private
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :password)
     end
 end
